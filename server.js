36091f60d9d4baca8dde7fb3978f33cea9566098
Adam Lassy <adamlassy@gmail.com>
2016-10-27 10:24:31 -0500

    up

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require('express');           // the express library
 var http = require("http");                 // the http library
-var WebSocketServer = require('ws').Server; // the ws library's Server class
 
 var server = express();                     // the express server
 var httpServer = http.createServer(server); // an http server
-var wss = new WebSocketServer({ server: httpServer }); // a websocket server
 
 var _count = 0;
 
