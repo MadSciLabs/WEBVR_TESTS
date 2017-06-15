bf686420fb3fd4386f4a0a71bb60c80142332ae9
Adam.Lassy <adam.lassy@US-NYC-C02T35EEHF1R.local>
2017-06-15 15:46:56 -0400

    Added hand stuff

--- aframe/fixChair/components/position-fix.js
+++ aframe/fixChair/components/position-fix.js
@@ -1,14 +1,25 @@
-var _objModel = document.querySelector('#obj-object');    
+var _objModel = document.querySelector('#obj-object'); 
+var _objHand = document.querySelector('#hand-object');
+
       window.addEventListener("keypress", moveObject);
         
-      function moveObject(event){ 
+      function moveObject(event) {
+
        var _x = _objModel.getAttribute('position').x;
        var _y = _objModel.getAttribute('position').y;
        var _z = _objModel.getAttribute('position').z;
        var _u = _objModel.getAttribute('rotation').x;
        var _v = _objModel.getAttribute('rotation').y;  
        var _w = _objModel.getAttribute('rotation').z;
-       console.log(_u, _v, _w);
+
+       var _hx = _objHand.getAttribute('position').x;
+       var _hy = _objHand.getAttribute('position').y;
+       var _hz = _objHand.getAttribute('position').z;
+       var _hu = _objHand.getAttribute('rotation').x;
+       var _hv = _objHand.getAttribute('rotation').y;  
+       var _hw = _objHand.getAttribute('rotation').z;
+
+       //console.log(_u, _v, _w);
           if(event.keyCode==49){
             _x+=0.05;
           }
@@ -36,9 +47,38 @@ var _objModel = document.querySelector('#obj-object');
           else if(event.keyCode==57){
             _w+=5; 
           }
+
+          //KEYCODE 
+          if(event.keyCode==108){
+            _hx+=0.05;
+          }
+          else if(event.keyCode==106){
+            _hx-=0.05;
+          }
+          else if(event.keyCode==105){
+            _hy+=0.05;
+          }
+          else if(event.keyCode==107){
+            _hy-=0.05;
+          }
+          else if(event.keyCode==117){
+            _hz+=0.05;
+          }
+          else if(event.keyCode==111){
+            _hz-=0.05;
+          }
+          if(event.keyCode==98){
+            _hu+=5;
+          }
+          else if(event.keyCode==110){
+            _hv+=5; 
+          }
+          else if(event.keyCode==109){
+            _hw+=5; 
+          }
           
-          console.log(_x, _y, _z);
-          console.log(_u, _v, _w);
+          console.log("Chair: " + _x + "," + _y + "," +_z + "-" + _u + "," + _v + "," + _w);
+          console.log("Hand: " + _hx + "," + _hy + "," +_hz + "-" + _hu + "," + _hv + "," + _hw);
           
         _objModel.setAttribute('position', `${_x} ${_y} ${_z}`);
         _objModel.setAttribute('rotation', `${_u} ${_v} ${_w}`);
